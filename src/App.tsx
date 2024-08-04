c66f941cf740d439c082e0e586bbaafd12d3bc10
Duball69 <120228301+duball69@users.noreply.github.com>
2024-08-04 07:55:59 +0100

    A

--- src/App.tsx
+++ src/App.tsx
@@ -174,7 +174,7 @@ const App: React.FC = () => {
         </div>
 
         <div className="flex-grow mt-4 bg-[#f3ba2f] rounded-t-[48px] relative top-glow z-0">
-          <div className="absolute top-[2px] left-0 right-0 bottom-0 bg-[#1d2025] rounded-t-[46px]">
+        <div className="background-image h-full flex flex-col">
             <div className="px-4 mt-6 flex justify-between gap-2">
               <div className="bg-[#272a2f] rounded-lg px-4 py-2 w-full relative">
                 <div className="dot"></div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      backgroundImage: {
+        "custom-bg": "url('https://imgur.com/0KJVRGq')", // Adjust the path as needed
+      },
+
       fontFamily: {
         bangers: ["Bangers", "system-ui"],
       },
